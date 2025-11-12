e63d14523086977fb2fac73e162f3c4ff3c1d323
Aakash Shrestha <aakash.cerestha90@gmail.com>
2025-11-12 17:00:48 +0545

    last commit

--- client/src/components/Achivement.jsx
+++ client/src/components/Achivement.jsx
@@ -1,7 +1,7 @@
 import { Carousel } from "antd";
 import SectionTitle from "./SectionTitle";
-import Lottie from "lottie-react";
-import certificate from "../assets/lottie/certificate.json";
+// import Lottie from "lottie-react";
+// import certificate from "../assets/lottie/certificate.json";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
@@ -53,9 +53,9 @@ function Achievement() {
         </div>
 
         {/* Lottie animation */}
-        <div className="h-[50vh] w-1/3 flex sm:w-full sm:h-[30vh] justify-end sm:hidden">
+        {/* <div className="h-[50vh] w-1/3 flex sm:w-full sm:h-[30vh] justify-end sm:hidden">
           <Lottie animationData={certificate} loop={true} autoPlay />
-        </div>
+        </div> */}
       </div>
     </div>
   );
