1942ae664af662ab5847dc7d0041f6639a7d5a5c
Aakash Shrestha <106760169+aakashstha1@users.noreply.github.com>
2025-11-12 17:16:35 +0545

    Update app.js

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://aakash-shrestha.onrender.com",
     credentials: true,
   })
 );
