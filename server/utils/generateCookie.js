355f7a370905e9fe22ad84fa646bb2c689d65eba
Aakash Shrestha <aakash.cerestha90@gmail.com>
2025-11-12 16:18:41 +0545

    fixed bugs

--- server/utils/generateCookie.js
+++ server/utils/generateCookie.js
@@ -2,13 +2,13 @@ import jwt from "jsonwebtoken";
 
 export const generateCookie = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
-    expiresIn: "7d",
+    expiresIn: "2h",
   });
 
   res.cookie("token", token, {
     httpOnly: true,
     sameSite: "strict",
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+    maxAge: 2 * 60 * 60 * 1000,
   });
 
   return token;
